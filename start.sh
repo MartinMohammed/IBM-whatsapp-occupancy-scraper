e13f8f965eafd7396ce99f4a1f582e78684bd276
Martin Mohammed <martin824616@gmail.com>
2023-06-17 00:15:52 +0200

    [skip ci] update start.sh script for mounting volumes and set grafana target version to 10.0.0

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Setup the volumes for Grafana:
+if [ ! -d "$PWD/grafana_data" ]; then  # Check if the grafana_data directory does not exist
+  docker volume create grafana_data    # Create a Docker volume named 'grafana_data'
+  docker volume inspect grafana_data   # Inspect the created volume for verification
+fi
+
+
+
 # The command -v command checks for the existence of the specified command and returns its path if found.
 if command -v docker compose >/dev/null 2>&1; then
     docker compose up --build
