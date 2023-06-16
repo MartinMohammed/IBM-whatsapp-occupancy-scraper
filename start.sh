51cc658fa114d339809069668f6f61b14a41b83e
Martin Mohammed <martin824616@gmail.com>
2023-06-17 01:21:25 +0200

    add docker-compose.dev.yml file for dev. and update start.sh script to pass --dev script in order to run docker-compose.dev.yml.

--- start.sh
+++ start.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# Setup the volumes for Grafana:
-if [ ! -d "$PWD/grafana_data" ]; then  # Check if the grafana_data directory does not exist
-  docker volume create grafana_data    # Create a Docker volume named 'grafana_data'
-  docker volume inspect grafana_data   # Inspect the created volume for verification
+# Read additional parameters.
+param1="$1"  # The first additional parameter.
+
+# Check if the 'docker' command exists.
+if command -v docker >/dev/null 2>&1; then
+  # Setup the volumes for Grafana:
+  if [ ! -d "$PWD/grafana_data" ]; then  # Check if the 'grafana_data' directory does not exist.
+    docker volume create grafana_data    # Create a Docker volume named 'grafana_data'.
+    docker volume inspect grafana_data   # Inspect the created volume for verification.
+  fi
+else
+  echo "The command 'docker' does not exist."
 fi
 
-
-
-# The command -v command checks for the existence of the specified command and returns its path if found.
-if command -v docker compose >/dev/null 2>&1; then
-    docker compose up --build
+# Check if the 'docker-compose' command exists.
+if command -v docker-compose >/dev/null 2>&1; then
+  if [ "$param1" == "--dev" ]; then  # Check if the additional parameter is "--dev".
+    docker-compose -f ./docker-compose.dev.yml up --build  # Start the services defined in the development Docker Compose file.
+  else
+    docker-compose -f ./docker-compose.yml up --build  # Start the services defined in the Docker Compose file.
+  fi
 else
-    echo "The command 'docker compose' does not exist."
+  echo "The command 'docker-compose' does not exist."
 fi
