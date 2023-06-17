35ac2481ad77cda3b3fc5e289213a65c7494360b
Martin Mohammed <martin824616@gmail.com>
2023-06-17 13:21:58 +0200

    [skip ci] update bash script to support '--clean' which will clean up all ressources used by containers.

--- start.sh
+++ start.sh
@@ -6,6 +6,12 @@ param1="$1"  # The first additional parameter.
 mkdir -p $PWD/app/data $PWD/app/logs $PWD/portainer_data $PWD/grafana_data
 chmod o+w $PWD/grafana_data
 
+# Check if the script is executed with the "--dev --clean" options
+if [[ "$1" == "--dev" && "$2" == "--clean" ]]; then
+  docker-compose -f docker-compose.dev.yml down -v
+  rm -rf $PWD/app/data $PWD/app/logs $PWD/postgres
+fi
+
 # Check if the 'docker-compose' command exists.
 if command -v docker compose >/dev/null 2>&1; then
   if [ "$param1" == "--dev" ]; then  # Check if the additional parameter is "--dev".
@@ -13,6 +19,12 @@ if command -v docker compose >/dev/null 2>&1; then
   else
     docker compose -f ./docker-compose.yml up --build  # Start the services defined in the Docker Compose file.
   fi
+
+  # Check if the script is executed with the "--dev --clean" options
+  if [[ "$1" == "--dev" && "$2" == "--clean" ]]; then
+    docker-compose -f docker-compose.dev.yml down -v
+  fi
+
 else
   echo "The command 'docker-compose' does not exist."
 fi
