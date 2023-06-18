0f65764dc211adeef73c17fe87cc9f1a17dcad28
Martin Mohammed <martin824616@gmail.com>
2023-06-18 12:59:46 +0200

    [skip ci] add exit statements in start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ if [[ "$1" == "--dev" && "$2" == "--clean" ]]; then
   # Clean up and remove volumes
   docker compose -f docker-compose.dev.yml down -v
   rm -rf "$PWD/app/data" "$PWD/app/logs" "$PWD/postgres"
+  exit 0
 fi
 
 # Check if the 'docker-compose' command exists.
@@ -18,26 +19,31 @@ if command -v docker-compose >/dev/null 2>&1 || command -v docker compose >/dev/
   if [[ "$1" == "--dev" && "$2" == "--clean" ]]; then
     # Clean up and remove volumes for development environment
     docker compose -f docker-compose.dev.yml down -v
+    exit 0 
   fi
 
   # Check if the script is executed with the "--dev --raspi" options
   if [[ "$1" == "--dev" && "$2" == "--raspi" ]]; then
     # Clean up and remove volumes for Raspberry Pi environment
     docker compose -f docker-compose.raspi.yml down -v
+    exit 0 
   fi
 
   if [ "$1" == "--dev" ]; then
     # Start the services defined in the development Docker Compose file.
     docker compose -f docker-compose.dev.yml up --build
+    exit 0
   elif [ "$1" == "--test" ]; then
     # Clean up before rebuilding visitor tracker
     rm -rf "$PWD/app/data" "$PWD/app/logs"
     docker compose build visitor-tracker
     # Run only the test script and remove the container afterward
     docker compose -f docker-compose.yml run --rm visitor-tracker sh -c "python3 test_runner.py"
+    exit 0
   else
     # Start the services defined in the Docker Compose file.
     docker compose -f docker-compose.yml up --build
+    exit 0
   fi
 
 
